55cd8f110d127c70d6f603189ee8d36b33f1e61d
MariaClaraCardosoo <mccardosorochadasilva@gmail.com>
2025-09-25 14:17:54 -0300

    25/09

---
+++ exercicio1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercicio e PHP</h1>
+    <form action="exercicio1.php">
+        <p>
+            Digite o nome do cliente<br>
+            <input type="text" nome="cliente">     
+        </p>
+        <p> 
+            escolha o sexo do cliente <br>
+            <input type="radio" nome="sexo" value="feminino"> Feminino<br>
+            <input type="radio" nome="sexo" value="masculino"> Masculino<br>
+        </p>
+        <p>
+            Digite o valor da compra <br>
+            <input type="number" nome="valorcompra">
+        </p>
+        <p>
+            <input type="submit" value="Enviar">
+
+        </form>
+</body>
+</html>
