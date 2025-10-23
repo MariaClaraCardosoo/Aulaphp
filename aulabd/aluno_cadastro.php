060f3cf06168cfaab1f22c12cf8da8e0838c17d4
MariaClaraCardosoo <mccardosorochadasilva@gmail.com>
2025-10-23 14:22:09 -0300

    23/10

---
+++ aulabd/aluno_cadastro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php include "menu.php"?>
+    <div class="conteudo">
+        <form action="#" method="post">
+            <p>
+                digite o nome do aluno<br>
+                <input type="text" name="nome">
+</p>
+<p>
+    Digite a cidade do aluno
+    <input type="text" name="cidade">
+</p>
+<p>
+    digite o estado
+    <input type="text" name="estado">
+
+    <p>
+        <input type="submit" name="enviar" value="Cadastrar">
+        
+
+</form>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    
+ <?php include "menu.php" ?>
+  
+
+    <div class="conteudo">
+        <h3>Integração de PHP com MySql</h3>
+        <p>Na aula de hoje, vamos estudar como integrar o PHP com o MySQL</p>
+    </div>
+</body>
+
+</html>
