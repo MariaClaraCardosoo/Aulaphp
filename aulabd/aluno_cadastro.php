2c61832dc6043538f5fd977f8e743c9ba3223e27
MariaClaraCardosoo <mccardosorochadasilva@gmail.com>
2025-10-30 11:32:47 -0300

    30/10

--- aulabd/aluno_cadastro.php
+++ aulabd/aluno_cadastro.php
@@ -10,22 +10,22 @@
 <body>
     <?php include "menu.php"?>
     <div class="conteudo">
-        <form action="#" method="post">
+        <form action="aluno_cadastro.php" method="post">
             <p>
                 digite o nome do aluno<br>
                 <input type="text" name="nome">
 </p>
 <p>
-    Digite a cidade do aluno
+    Digite a cidade do aluno<br>
     <input type="text" name="cidade">
 </p>
 <p>
-    digite o estado
+    digite o estado<br>
     <input type="text" name="estado">
-
+</p>
     <p>
         <input type="submit" name="enviar" value="Cadastrar">
-        
+</p>
 
 </form>
 </div>
